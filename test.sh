85c7393e3bfb5418533acfbb7ccb60e40d823dd5
Prakash23 <nepal@23.com>
2024-12-03 20:11:35 +0545

    file added

---
+++ test.sh
@@ -0,0 +1,7 @@
+# systemctl status apache2.service 
+● apache2.service - The Apache HTTP Server
+   Loaded: loaded (/lib/systemd/system/apache2.service; 
+  Drop-In: /lib/systemd/system/apache2.service.d
+           └─apache2-systemd.conf
+   Active: failed (Result: exit-code) since Wed 2020-03-11 23:17:35 WIB; 13s ago
+  Process: 9151 ExecStart=/usr/sbin/apachectl start (code=exited, status=139)
